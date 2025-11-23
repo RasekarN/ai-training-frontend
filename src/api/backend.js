75f1dfa73c02e7163b0438e3c778bbc679066c59
RasekarN <72785812+RasekarN@users.noreply.github.com>
2025-11-23 22:00:40 +0530

    Update backend.js

--- src/api/backend.js
+++ src/api/backend.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://ai-training-backend-c67c.onrender.com",
 });
 
 export const getSignal = async (payload) => {
